074c7d70f6e5e4b61f7d91647677285594802df3
danyalutsevich <luchevich31@gmail.com>
2023-04-08 14:28:23 +0300

    Directions controller

---
+++ Firefly-Lines/Controllers/DirectionsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Firefly_Lines.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class DirectionsController : Controller
+	{
+		[HttpGet(Name = "GetDirections")]
+		public IActionResult Get()
+		{
+			return new JsonResult(new { Message = "Hello from DirectionsController" });
+		}
+	}
+}
